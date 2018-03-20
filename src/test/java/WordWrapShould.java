b741842c5ef851a3d3a8d9a727e8f68ef8b5e50d
carlosble <carlos@codesai.com>
2018-03-20 16:13:39 +0100

    adding exercise

---
+++ src/test/java/WordWrapShould.java
@@ -0,0 +1,22 @@
+import org.junit.Test;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/*
+    "hola", 7 -> "hola"
+    "hola", 2 -> "ho\nla"
+    "hola mundo", 7 -> "hola\n mundo"
+ */
+public class WordWrapShould {
+
+
+    @Test
+    public void wrap () {
+
+    }
+
+    private String wordWrap(String text, int columns){
+        throw new NotImplementedException();
+    }
+}
