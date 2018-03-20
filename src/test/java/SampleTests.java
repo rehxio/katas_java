bfea67e61da75f75c4e0f4014d4a7f8cd3644837
carlosble <carlos@codesai.com>
2018-03-20 08:56:01 +0100

    skeleton

---
+++ src/test/java/SampleTests.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+import static org.assertj.core.api.Assertions.*;
+
+public class SampleTests {
+
+    @Test
+    public void it_works (){
+        assertThat(1).isEqualTo(1);
+    }
+
+}
