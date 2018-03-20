041729635c2f47edbd65c1700ea055ddad32331a
carlosble <carlos@codesai.com>
2018-03-20 15:14:16 +0100

    adding more exercises

---
+++ src/test/java/StringCalculatorShould.java
@@ -0,0 +1,24 @@
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/*
+ String Calculator Kata - Roy Oshevore
+ Recommended Book:
+ Lasse Koskela - Effective Unit Testing
+
+ Exercise:
+ public int f(String x);
+
+ TODO-list:
+ "1" -> 1
+ "" -> 0
+ "1,1"-> 2
+ "2,2,2"-> 6
+ "abc" -> 0
+ */
+
+public class StringCalculatorShould {
+
+
+}
