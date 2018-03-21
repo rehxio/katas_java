328f5deea209c064240c241e6c96b303828d58da
carlosble <carlos@codesai.com>
2018-03-21 11:24:16 +0100

    adding exercise

---
+++ src/test/java/PasswordValidatorShould.java
@@ -0,0 +1,20 @@
+/*
+ Exercise:
+ public boolean f(String x);
+
+ Acceptance criteria:
+ A valid password should contain:
+    - at least one lower case char
+    - at least one upper case char
+    - at least one number
+    - at least one underscore
+    - at least 6 characters
+
+ TODO-list:
+ ???
+ */
+
+public class PasswordValidatorShould {
+
+
+}
